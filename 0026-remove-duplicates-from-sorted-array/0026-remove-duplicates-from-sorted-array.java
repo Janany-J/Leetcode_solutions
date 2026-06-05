f02bb8ad41178f9b33c62a3d5288f91946079edb
Janany J <janany7024@gmail.com>
2026-06-05 11:50:44 +0530

    Time: 1 ms (76.82%), Space: 46.8 MB (22.38%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,8 +3,8 @@ class Solution {
         int i=0;
         for(int j=1;j<nums.length;j++){
             if(nums[j]!=nums[i]){
-                i++;
-                nums[i]=nums[j];
+                
+                nums[++i]=nums[j];
             }
         }
         return i+1;
