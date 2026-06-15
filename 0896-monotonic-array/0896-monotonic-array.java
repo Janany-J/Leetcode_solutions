2df1925508ddc25d4d13dd597c99dc59bd714642
Janany J <janany7024@gmail.com>
2026-06-15 14:04:49 +0530

    Time: 2 ms (63.43%), Space: 85.2 MB (38.23%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -3,13 +3,13 @@ class Solution {
         boolean increasing = true;
         boolean decreasing = true;
         for(int i=1;i<nums.length;i++){
-            if(nums[i]>nums[i-1]){
-                decreasing = false;
-            }
             if(nums[i]<nums[i-1]){
                 increasing = false;
             }
+            if(nums[i]>nums[i-1]){
+                decreasing = false;
+            }
         }
-        return decreasing || increasing;
+        return increasing || decreasing;
     }
 }
